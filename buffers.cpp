afff86a7bee5eb8cb337a15f3fc63d0a0b6c8dfa
RahulVadhyar <vadhyarrahul@gmail.com>
2023-10-24 17:23:20 +0530

    Time to render 2 rectangles. Wish me luck future person going through these commits!!

--- buffers.cpp
+++ buffers.cpp
@@ -42,7 +42,7 @@ void UniformBuffer::update(VkExtent2D swapChainExtent) {
 
 	UniformBufferObject ubo{};
 	ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-	ubo.view = glm::lookAt(glm::vec3(0.0f, 0.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	ubo.view = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.1f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 	ubo.proj = glm::ortho(-1.0f, 1.0f, -(float)swapChainExtent.height / (float)swapChainExtent.width, (float)swapChainExtent.height/(float)swapChainExtent.width, -100.0f, 100.0f);
 	ubo.proj[1][1] *= -1;
 	memcpy(data, &ubo, sizeof(ubo));
