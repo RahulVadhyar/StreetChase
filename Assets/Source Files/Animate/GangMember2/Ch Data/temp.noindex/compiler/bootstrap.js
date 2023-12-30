3b1768907baeeccdc7ac4d8a6cb5c791abaa143e
RahulVadhyar <vadhyarrahul@gmail.com>
2023-12-30 18:29:12 +0530

    Started with vulkan from scratch, enjoy later commit messages

--- Assets/Source Files/Animate/GangMember2/Ch Data/temp.noindex/compiler/bootstrap.js
+++
@@ -1,3 +0,0 @@
-var __bootStrapPath = _modulePath;
-var __compiledScene = _cwd + "\\scene.json";
-runScript("C:\\Program Files\\Adobe\\Adobe Character Animator 2023\\Support Files\\Runtime\\animal\\scripts\\js\\lib\\engine.js")
--- Weapons/simplebullet.hpp
+++
@@ -1,9 +0,0 @@
-class SimpleBullet: public BaseBulletClass{
-    public:
-        SimpleBullet(float x, float y):
-        BaseBulletClass::BaseBulletClass(x, y, 0.03, 0.03, BULLET_TEX_DIR){
-            damage = 0.3;
-            initial_velocity = 0.005;
-        }
-
-};
