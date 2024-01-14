d2072cc31c35d81790202937a51293e342998e5d
RahulVadhyar <vadhyarrahul@gmail.com>
2024-01-15 02:25:27 +0530

    Relative paths and sync the chronos commit

--- Source/Source/main.cpp
+++ Source/Source/main.cpp
@@ -1,6 +1,10 @@
 #include "chronos.hpp"
 
 int main(){
+
+    std::filesystem::path cwdpath("../");
+    std::cout << cwdpath << std::endl;
+    std::filesystem::current_path(cwdpath);
     Chronos::Manager::Initializer initializer;
     initializer.WindowWidth = 800;
     initializer.WindowHeight = 600;
@@ -10,7 +14,7 @@ int main(){
 
     Chronos::Manager::Manager manager(initializer);
     
-    ShapeParams triangleParams;
+    Chronos::Engine::ShapeParams triangleParams;
     triangleParams.x = 0.5;
     triangleParams.y = 0.5;
     triangleParams.xSize = 0.5;
@@ -18,9 +22,9 @@ int main(){
     triangleParams.rotation = 0;
     Chronos::Manager::PolygonType triangleType;
     triangleType.triangle = true;
-    int triangle = manager.addPolygon(triangleParams, triangleType, "../Assets/texture.jpg");
+    int triangle = manager.addPolygon(triangleParams, triangleType, "Assets/texture.jpg");
 
-    ShapeParams rectangleParams;
+    Chronos::Engine::ShapeParams rectangleParams;
     rectangleParams.x = -0.5;
     rectangleParams.y = -0.5;
     rectangleParams.xSize = 0.5;
@@ -28,7 +32,7 @@ int main(){
     rectangleParams.rotation = 0;
     Chronos::Manager::PolygonType rectangleType;
     rectangleType.rectangle = true;
-    int rectangle = manager.addPolygon(rectangleParams, rectangleType, "../Assets/texture.jpg");
+    int rectangle = manager.addPolygon(rectangleParams, rectangleType, "Assets/texture.jpg");
 
     Chronos::Manager::Text text;
     text.x = 0.0;
@@ -42,9 +46,6 @@ int main(){
         }
         manager.drawFrame();
     }
-    if(manager.stop() == 0){
-        throw std::runtime_error("Chronos::Manager::stop() failed");
-    };
     
     return 0;
 }
